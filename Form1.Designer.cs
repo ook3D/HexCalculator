f79d30e8a92d32bd9829a9bd68abf49177a9b18c
ook3d <47336113+ook3D@users.noreply.github.com>
2025-08-25 12:19:34 -0400

    resize form size

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -56,7 +56,7 @@
             checkedListBox1.Items.AddRange(new object[] { "0x00000001 - Flags2", "0x00000002 - ParentOverrides", "0x00000004 - Volume", "0x00000008 - VolumeVariance", "0x00000010 - Pitch", "0x00000020 - PitchVariance", "0x00000040 - Pan", "0x00000080 - PanVariance", "0x00000100 - PreDelay", "0x00000200 - PreDelayVariance", "0x00000400 - StartOffset", "0x00000800 - StartOffsetVariance", "0x00001000 - AttackTime", "0x00002000 - ReleaseTime", "0x00004000 - DopplerFactor", "0x00008000 - Category", "0x00010000 - LPFCutOff", "0x00020000 - LPFCutOffVariance", "0x00040000 - HPFCutOff", "0X00080000 - HPFCutOffVariance", "0x00100000 - VolumeCurve", "0x00200000 - VolumeCurveScale", "0x00400000 - VolumeCurvePlateau", "0x00800000 - SpeakerMask", "0x01000000 - EffectRoute", "0x02000000 - PreDelayVariable", "0x04000000 - StartOffsetVariable", "0x08000000 - SmallReverbSend", "0x10000000 - MediumReverbSend", "0x20000000 - LargeReverbSend" });
             checkedListBox1.Location = new Point(6, 95);
             checkedListBox1.Name = "checkedListBox1";
-            checkedListBox1.Size = new Size(340, 562);
+            checkedListBox1.Size = new Size(340, 580);
             checkedListBox1.TabIndex = 6;
             checkedListBox1.ItemCheck += checkedListBox1_ItemCheck;
             checkedListBox1.MouseDown += checkedListBox1_MouseDown;
@@ -65,7 +65,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(352, 668);
+            ClientSize = new Size(352, 680);
             Controls.Add(checkedListBox1);
             Controls.Add(OutputHexTextBox);
             FormStyle = FormStyles.ActionBar_None;
